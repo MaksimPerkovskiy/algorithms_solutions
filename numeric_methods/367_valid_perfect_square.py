4499a255957d73e4bc1f65a94f684edec45f6355
Maksim Perkovskiy <m.perkovskiy@godeltech.com>
2023-05-05 12:42:28 +0200

    367_task_solved

---
+++ numeric_methods/367_valid_perfect_square.py
@@ -0,0 +1,17 @@
+def is_perfect_square(number: int) -> bool:
+    start = 0
+    end = number
+    while (end - start) != 1:
+        mid = (end + start) // 2
+        to_check = mid * mid
+        if to_check > number:
+            end = mid
+        elif to_check < number:
+            start = mid
+        else:
+            return True
+
+    return False
+
+
+print(is_perfect_square(2374681))
